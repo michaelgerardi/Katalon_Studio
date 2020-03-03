058a327bc4cbc9d502d19bcebd60189daeccf496
michaelgerardi <mikelgerard17@gmail.com>
2020-03-03 18:40:37 +0700

    initial commit

---
+++ Test Suites/Test Login.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Menguji Login dengan data yang benar</description>
+   <name>Test Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b11a74ad-6528-451d-b858-6960da5d98a5</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef248dca-eb64-4580-ae77-a91cd8080a67</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Login Dian dan Kopi</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ebfdf69-0234-4724-bf4b-eb2d1077016e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Login Dians dan Kopi</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
